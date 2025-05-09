b503ec256a079727feaa93141db8471cbe6b82f0
Zahran <zahransammi2005@gmail.com>
2025-05-09 09:20:32 +0700

    perubahan ke 2

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -11,12 +11,7 @@ const Projects = () => {
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-        href="projects-folder/website-sederhana/"
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
+      
     
       </div>
     </div>
